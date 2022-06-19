b14839a226b97512f32d7cc275037acb1477b3f8
Ilya758 <subscriber6436@gmail.com>
2022-06-19 13:13:59 +0300

    fix: fixed webpack.config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,35 +3,22 @@ const path = require('path');
 const PATHS = {
   src: path.join(__dirname, './src'),
   dist: path.join(__dirname, './dist'),
-  outputJS: './js/',
-  outputCSS: './css/',
 };
 
 module.exports = {
   context: PATHS.src,
-  mode: 'development',
+  mode: 'production',
   entry: {
-    main: ['@babel/polyfill', './index.ts'],
+    main: './index.ts',
   },
   output: {
-    filename: `${PATHS.outputJS}[name].js`,
+    filename: `[name].cjs`,
     path: PATHS.dist,
   },
+  target: 'node',
   resolve: {
     extensions: ['.js', '.json', '.ts'],
-    alias: {},
   },
-  devServer: {
-    port: 8081,
-    static: PATHS.src,
-    client: {
-      overlay: {
-        warnings: false,
-        errors: false,
-      },
-    },
-  },
-  plugins: [],
   module: {
     rules: [
       {
