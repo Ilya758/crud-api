c3c92ed2407198f16ab5ac4008383dd3f5ac6667
Ilya758 <subscriber6436@gmail.com>
2022-06-19 15:39:36 +0300

    fix: minor edit with index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,220 +13,222 @@ import { findUserById } from './utils/findUserById';
 
 dotenv.config({ path: resolve(cwd(), '.env') });
 
-http
-  .createServer((request, response) => {
-    switch (request.method) {
-      case Method.GET: {
-        if (request.url === Paths.getUsers) {
-          response.statusCode = StatusCode.success;
-          response.end(JSON.stringify(UserService.users, null, 2));
-          break;
-        }
+export const server = http.createServer((request, response) => {
+  switch (request.method) {
+    case Method.GET: {
+      if (request.url === Paths.getUsers) {
+        response.statusCode = StatusCode.success;
+        response.end(JSON.stringify(UserService.users, null, 2));
+        break;
+      }
 
-        if (request.url?.split('/')[3]) {
-          const userId = request.url?.split('/')[3];
+      if (request.url?.split('/')[3]) {
+        const userId = request.url?.split('/')[3];
 
-          const user = findUserById(userId, UserService.users);
+        const user = findUserById(userId, UserService.users);
 
-          if (!userId || userId.length !== 36) {
-            response.statusCode = StatusCode.badRequest;
-            response.end(
-              JSON.stringify({
-                code: StatusCode.badRequest,
-                message: 'Invalid userId',
-              })
-            );
-          } else if (!user) {
-            response.statusCode = StatusCode.notFound;
-            response.end(
-              JSON.stringify({
-                code: StatusCode.notFound,
-                message: 'User not found!',
-              })
-            );
-
-            break;
-          }
-
-          response.statusCode = StatusCode.success;
-          response.end(JSON.stringify(user));
-        } else {
+        if (!userId || userId.length !== 36) {
+          response.statusCode = StatusCode.badRequest;
+          response.end(
+            JSON.stringify({
+              code: StatusCode.badRequest,
+              message: 'Invalid userId',
+            })
+          );
+        } else if (!user) {
           response.statusCode = StatusCode.notFound;
           response.end(
             JSON.stringify({
               code: StatusCode.notFound,
-              message: 'This url does not exist!',
+              message: 'User not found!',
             })
           );
 
           break;
         }
 
-        break;
-      }
-
-      case Method.POST: {
-        switch (request.url) {
-          case Paths.getUsers: {
-            let data = '';
-
-            request.on('data', (chunk: Buffer) => {
-              data += chunk;
-            });
-
-            request.on('end', () => {
-              const user = JSON.parse(data) as IUser;
-              user.id = v4();
-
-              const isUserFieldsValid = checkUserFieldsExistence(
-                Object.keys(user)
-              );
-
-              if (isUserFieldsValid) {
-                response.statusCode = StatusCode.successfullyCreated;
-
-                UserService.setNewUser(user);
-
-                response.end(JSON.stringify(user));
-              } else {
-                response.statusCode = StatusCode.badRequest;
-
-                response.end(
-                  JSON.stringify({
-                    code: StatusCode.badRequest,
-                    error:
-                      "body doesn't contain valid fields or there're not exist",
-                  })
-                );
-              }
-            });
-
-            break;
-          }
-
-          default:
-            break;
-        }
+        response.statusCode = StatusCode.success;
+        response.end(JSON.stringify(user, null, 2));
+      } else {
+        response.statusCode = StatusCode.notFound;
+        response.end(
+          JSON.stringify({
+            code: StatusCode.notFound,
+            message: 'This url does not exist!',
+          })
+        );
 
         break;
       }
 
-      case Method.PUT: {
-        if (request.url?.split('/')[3]) {
-          let data = '';
-
-          request.on('data', (chunk: Buffer) => {
-            data += chunk;
-          });
-
-          request.on('end', () => {
-            const user = JSON.parse(data) as IUser;
-
-            const isUserFieldsValid = checkUserFieldsExistence(
-              Object.keys(user)
-            );
-
-            if (isUserFieldsValid) {
-              const userId = request.url?.split('/')[3] as string;
-
-              if (!userId || userId.length !== 36) {
-                response.statusCode = StatusCode.badRequest;
-
-                response.end(
-                  JSON.stringify({
-                    code: StatusCode.badRequest,
-                    message: 'Invalid userId',
-                  })
-                );
-              } else if (!user) {
-                response.statusCode = StatusCode.notFound;
-                response.end(
-                  JSON.stringify({
-                    code: StatusCode.notFound,
-                    message: 'User not found!',
-                  })
-                );
-              }
-
-              UserService.updateUserById(userId, user);
-
-              const updatedUser = { ...user, id: userId };
-
-              response.statusCode = StatusCode.success;
-              response.end(JSON.stringify(updatedUser));
-            }
-          });
-        } else {
-          response.statusCode = StatusCode.notFound;
-          response.end(
-            JSON.stringify({
-              code: StatusCode.notFound,
-              message: 'This url does not exist!',
-            })
-          );
-        }
-
-        break;
-      }
-
-      case Method.DELETE: {
-        if (request.url?.split('/')[3]) {
-          let data = '';
-
-          request.on('data', (chunk: Buffer) => {
-            data += chunk;
-          });
-
-          request.on('end', () => {
-            const user = JSON.parse(data) as IUser;
-
-            const isUserFieldsValid = checkUserFieldsExistence(
-              Object.keys(user)
-            );
-
-            if (isUserFieldsValid) {
-              const userId = request.url?.split('/')[3] as string;
-
-              if (!userId || userId.length !== 36) {
-                response.statusCode = StatusCode.badRequest;
-
-                response.end(
-                  JSON.stringify({
-                    code: StatusCode.badRequest,
-                    message: 'Invalid userId',
-                  })
-                );
-              } else if (!user) {
-                response.statusCode = StatusCode.notFound;
-                response.end(
-                  JSON.stringify({
-                    code: StatusCode.notFound,
-                    message: 'User not found!',
-                  })
-                );
-              }
-
-              UserService.deleteUserById(userId);
-
-              response.statusCode = StatusCode.noContent;
-              response.end();
-            }
-          });
-        } else {
-          response.statusCode = StatusCode.notFound;
-          response.end(
-            JSON.stringify({
-              code: StatusCode.notFound,
-              message: 'This url does not exist!',
-            })
-          );
-        }
-
-        break;
-      }
-
-      default:
-        break;
+      break;
     }
-  })
-  .listen(process.env.PORT);
+
+    case Method.POST: {
+      switch (request.url) {
+        case Paths.getUsers: {
+          let data = '';
+
+          request.on('data', (chunk: Buffer) => {
+            data += chunk;
+          });
+
+          request.on('end', () => {
+            const user = JSON.parse(data) as IUser;
+            user.id = v4();
+
+            const isUserFieldsValid = checkUserFieldsExistence(
+              Object.keys(user)
+            );
+
+            if (isUserFieldsValid) {
+              response.statusCode = StatusCode.successfullyCreated;
+
+              UserService.setNewUser(user);
+
+              response.end(JSON.stringify(user, null, 2));
+            } else {
+              response.statusCode = StatusCode.badRequest;
+
+              response.end(
+                JSON.stringify({
+                  code: StatusCode.badRequest,
+                  error:
+                    "body doesn't contain valid fields or there're not exist",
+                })
+              );
+            }
+          });
+
+          break;
+        }
+
+        default:
+          break;
+      }
+
+      break;
+    }
+
+    case Method.PUT: {
+      if (request.url?.split('/')[3]) {
+        let data = '';
+
+        request.on('data', (chunk: Buffer) => {
+          data += chunk;
+        });
+
+        request.on('end', () => {
+          const user = JSON.parse(data) as IUser;
+
+          const isUserFieldsValid = checkUserFieldsExistence(Object.keys(user));
+
+          if (isUserFieldsValid) {
+            const userId = request.url?.split('/')[3] as string;
+
+            if (!userId || userId.length !== 36) {
+              response.statusCode = StatusCode.badRequest;
+
+              response.end(
+                JSON.stringify({
+                  code: StatusCode.badRequest,
+                  message: 'Invalid userId',
+                })
+              );
+            } else if (!user) {
+              response.statusCode = StatusCode.notFound;
+              response.end(
+                JSON.stringify({
+                  code: StatusCode.notFound,
+                  message: 'User not found!',
+                })
+              );
+            }
+
+            UserService.updateUserById(userId, user);
+
+            const updatedUser = { ...user, id: userId };
+
+            response.statusCode = StatusCode.success;
+            response.end(JSON.stringify(updatedUser, null, 2));
+          }
+        });
+      } else {
+        response.statusCode = StatusCode.notFound;
+        response.end(
+          JSON.stringify({
+            code: StatusCode.notFound,
+            message: 'This url does not exist!',
+          })
+        );
+      }
+
+      break;
+    }
+
+    case Method.DELETE: {
+      if (request.url?.split('/')[3]) {
+        let data = '';
+
+        request.on('data', (chunk: Buffer) => {
+          data += chunk;
+        });
+
+        request.on('end', () => {
+          const user = JSON.parse(data) as IUser;
+
+          const isUserFieldsValid = checkUserFieldsExistence(Object.keys(user));
+
+          if (isUserFieldsValid) {
+            const userId = request.url?.split('/')[3] as string;
+
+            if (!userId || userId.length !== 36) {
+              response.statusCode = StatusCode.badRequest;
+
+              response.end(
+                JSON.stringify({
+                  code: StatusCode.badRequest,
+                  message: 'Invalid userId',
+                })
+              );
+            } else if (!user) {
+              response.statusCode = StatusCode.notFound;
+              response.end(
+                JSON.stringify({
+                  code: StatusCode.notFound,
+                  message: 'User not found!',
+                })
+              );
+            }
+
+            UserService.deleteUserById(userId);
+
+            response.statusCode = StatusCode.noContent;
+            response.end();
+          }
+        });
+      } else {
+        response.statusCode = StatusCode.notFound;
+        response.end(
+          JSON.stringify({
+            code: StatusCode.notFound,
+            message: 'This url does not exist!',
+          })
+        );
+      }
+
+      break;
+    }
+
+    default:
+      break;
+  }
+});
+
+export const listen = () => {
+  server.listen(process.env.PORT);
+};
+
+export const close = () => server.close();
+
+listen();
