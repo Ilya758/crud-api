c4c13f51c6ffc2a3b58eaa39873d0eacef347450
Ilya758 <subscriber6436@gmail.com>
2022-06-19 13:15:11 +0300

    feat: implemented enum constants

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+export enum Paths {
+  getUsers = '/api/users',
+}
+
+export enum Method {
+  DELETE = 'DELETE',
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+}
+
+export enum StatusCode {
+  success = 200,
+  successfullyCreated = 201,
+  noContent = 204,
+  badRequest = 400,
+  notFound = 404,
+}
